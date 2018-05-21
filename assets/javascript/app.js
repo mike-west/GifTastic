4314c5c7686e0a8851fdfcaa369051ce8aca2c9c
Mike West <mister270@gmail.com>
2018-05-20 22:04:06 -0700

    don't know why it took so long to download and place the still gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
 var topics = ['NASCAR', 'Formula 1', 'Open Wheel Racng', 'Indianapolis 500', 'Legends Car Racing', 'Funny Car Drag Racing', 'Top Fuel Drag Racing',
     'Motorcycle Racing', 'Snowmobile Racing', 'Drag Boat Racing', 'Sailboat Racing'];
 
+var isStill = [];
+
 var queryURL = "https://api.giphy.com/v1/gifs/search?";
 
 var queryParams = {
@@ -13,6 +15,17 @@ function makeQueryString(topic) {
     return queryURL + $.param(queryParams);
 }
 
+function updateRaceGifs(data) {
+    $("#race-gifs").empty();
+    for(var i=0; i < data.length; ++i) {
+        var imgUrl = data[i].images.original_still.url;
+        var img = $("<img>").attr("src", imgUrl);
+        
+        $("#race-gifs").append(img);
+    }
+
+}
+
 window.onload = function (event) {
 
     // go through the topics and load buttons up
@@ -23,17 +36,22 @@ window.onload = function (event) {
     }
 
     $(".racer-btn").on("click", function() {
+        // build query string
         var query = makeQueryString($(this).text());
         console.log(query);
-        getGifs(query);
+
+        // make ajax call
+        $.ajax({
+            url: query,
+            method: "GET"
+        }).then(function (resp) {
+            console.log(resp.data);
+            updateRaceGifs(resp.data);
+        });
+
+
+ 
     });
 }
 
-function getGifs(query) {
-    $.ajax({
-        url: query,
-        method: "GET"
-    }).then(function (resp) {
-        console.log(resp);
-    });
-}
+
